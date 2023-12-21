830a84bb86f4b8e35fe0dee72de106ce9bc0586c
qiaoqiaoLF <qwerewqwer4@gmail.com>
2023-12-21 22:47:42 +0800

    Modify Code

--- chamfer_distance.py
+++ chamfer_distance.py
@@ -2,47 +2,46 @@ import open3d as o3d
 import numpy as np
 from scipy.spatial import cKDTree
 import random
-from chamfer_distance_utils import load_and_normalize_mesh, align_meshes,align_meshes_Kabsch_Umeyama_algorithm, rotate_mesh_points, visualize_meshes, normalize_mesh_scale, chamfer_distance
-
+from chamfer_distance_utils import *
 def main():
     filename = 'polaroid_99'
     
     file1, file2 = f'./data/prediction/{filename}.ply', f'./data/ground_truth/{filename}.ply'
-    mesh1_points, mesh2_points = load_and_normalize_mesh(file1,number_of_points=5000), load_and_normalize_mesh(file2,number_of_points=5000)
+    mesh1,mesh2 = load_mesh(file1),load_mesh(file2)
     print(f"Loaded {file1} and {file2}")
-    if mesh1_points is None or mesh2_points is None:
+    if mesh1 is None or mesh2 is None:
         print("Error loading mesh files.")
         return
+    
+    # Normalize meshes
+    mesh1, mesh2 = normalize_mesh_scale(mesh1), normalize_mesh_scale(mesh2)
 
-    # Normalize mesh scales
-    mesh1_points, mesh2_points = normalize_mesh_scale(mesh1_points), normalize_mesh_scale(mesh2_points)
 
     # Visualize original meshes
-    visualize_meshes(mesh1_points, mesh2_points, "Original Meshes")
+    visualize_meshes(mesh1, mesh2, "Original Meshes")
+    # sample points from meshes
+    mesh1_points, mesh2_points = sample_points_from_mesh(mesh1), sample_points_from_mesh(mesh2)
+    # get points from point cloud objects
+    mesh1_points, mesh2_points = np.asarray(mesh1_points.points), np.asarray(mesh2_points.points)
     distance_original = chamfer_distance(mesh1_points, mesh2_points)
     print(f"Chamfer Distance before random rotation: {distance_original}")
-    
-    
-    # Apply random rotation
-    random_angle, random_axis = random.uniform(0, 2 * np.pi), np.random.rand(3)
-    rotated_mesh1_points = rotate_mesh_points(mesh1_points, random_angle, random_axis)
 
-    # Visualize after rotation
-    visualize_meshes(rotated_mesh1_points, mesh2_points, "Meshes After Random Rotation")
-
-    # Chamfer Distance before alignment
-    distance_pre_align = chamfer_distance(rotated_mesh1_points, mesh2_points)
-    print(f"Chamfer Distance after random rotation: {distance_pre_align}")
 
     # Align meshes
-    aligned_mesh1_points, transformation_matrix = align_meshes_Kabsch_Umeyama_algorithm(rotated_mesh1_points, mesh2_points)
+    transformation_matrix = align_meshes( mesh1, mesh2, number_of_points=5000)
+    # Apply the transformation to mesh1
+    aligned_mesh1 = transform_mesh(mesh1, transformation_matrix)
     # output the transformation matrix as well
     print(f"Transformation Matrix:\n{transformation_matrix}")
     # Visualize after alignment
-    visualize_meshes(aligned_mesh1_points, mesh2_points, "Meshes After Alignment")
+    visualize_meshes(aligned_mesh1, mesh2, "Meshes After Alignment")
 
+    # sample points from meshes
+    aligned_mesh1_points, mesh2_points = sample_points_from_mesh(aligned_mesh1), sample_points_from_mesh(mesh2)
+    # get points from point cloud objects
+    aligned_mesh1_points, mesh2_points = np.asarray(aligned_mesh1_points.points), np.asarray(mesh2_points.points)
     # Chamfer Distance after alignment
-    distance_post_align = chamfer_distance(aligned_mesh1_points, mesh2_points)
+    distance_post_align = chamfer_distance( aligned_mesh1_points, mesh2_points)
     print(f"Chamfer Distance after alignment: {distance_post_align}")
 
 if __name__ == "__main__":
--- chamfer_distance_utils.py
+++ chamfer_distance_utils.py
@@ -1,22 +1,14 @@
 import open3d as o3d
 import numpy as np
 from scipy.spatial import cKDTree
-import random
 import scipy
 
-def load_and_normalize_mesh(file_name, number_of_points=10000):
+def load_mesh(file_name):
     try:
         mesh = o3d.io.read_triangle_mesh(file_name)
         mesh.compute_vertex_normals() if not mesh.has_vertex_normals() else None
         mesh.compute_triangle_normals() if not mesh.has_triangle_normals() else None
-
-        scale_factor = 1 / np.max(mesh.get_max_bound() - mesh.get_min_bound())
-        mesh.scale(scale_factor, center=mesh.get_center())
-
-        # Sample points from the mesh surface using Poisson Disk Sampling
-        pcd = mesh.sample_points_poisson_disk(number_of_points)
-
-        return np.asarray(pcd.points)
+        return mesh
     except Exception as e:
         print(f"Error loading mesh from {file_name}: {e}")
         return None
@@ -25,6 +17,8 @@ def kabsch_umeyama(P, Q):
     """
     The Kabsch-Umeyama algorithm to find the optimal rotation and translation
     to align two sets of points P and Q.
+    
+    BUG It is not suitable for our case because our points are not correspondences.
 
     Parameters:
     P (numpy.ndarray): First set of points (Nx3).
@@ -62,136 +56,126 @@ def kabsch_umeyama(P, Q):
     # Step 6: Apply the rotation and translation to P
     P_aligned = P @ R + t
 
-    return P_aligned, R, t
+    return P_aligned, R, t  
+    
+def transfer_normals_from_mesh_to_point_cloud(mesh, point_cloud):
+    # Ensure the mesh has triangle normals
+    # BUG
+    if not mesh.has_triangle_normals():
+        mesh.compute_triangle_normals()
+
+    # Create a KDTree for the mesh triangles (using triangle centroids)
+    triangle_centroids = np.mean(np.asarray(mesh.vertices)[np.asarray(mesh.triangles), :], axis=1)
+    mesh_tree = o3d.geometry.KDTreeFlann(triangle_centroids)
+
+    # Preallocate array for normals
+    normals = np.zeros((len(point_cloud.points), 3))
+
+    # Find the closest triangle in the mesh for each point in the point cloud
+    for i, point in enumerate(point_cloud.points):
+        _, idx, _ = mesh_tree.search_knn_vector_3d(point, 1)
+        normals[i] = mesh.triangle_normals[idx[0]]
+
+    # Assign computed normals to the point cloud
+    point_cloud.normals = o3d.utility.Vector3dVector(normals)
 
 
-def align_meshes_Kabsch_Umeyama_algorithm(source_points, target_points):
+def align_meshes(source_mesh, target_mesh, max_correspondence_distance=0.5, initial_transformation=np.eye(4), use_point_to_plane=True, relative_fitness=0.000001, relative_rmse=0.000001, max_iteration=3000, number_of_points=1000):
     """
-    Aligns the source points to the target points using the Kabsch-Umeyama algorithm.
+    Aligns the source mesh to the target mesh using the ICP algorithm.
 
-    :param source_points: NumPy array of source points.
-    :param target_points: NumPy array of target points.
-    :return: Aligned source point cloud as a numpy array.
-    """
-    # Apply the Kabsch-Umeyama algorithm
-    aligned_source_points, rotation_matrix, translation_vector = kabsch_umeyama(source_points, target_points)
-    transformation_matrix = np.eye(4)
-    transformation_matrix[:3, :3] = rotation_matrix
-    transformation_matrix[:3, 3] = translation_vector
-    
-    return aligned_source_points,  transformation_matrix
-    
-    
-    
-def align_meshes(source_points, target_points, threshold=0.02, max_iterations=2000, initial_transformation=None, use_point_to_plane=False):
-    """
-    Aligns the source points to the target points using the ICP algorithm.
-
-    :param source_points: NumPy array of source points.
-    :param target_points: NumPy array of target points.
-    :param threshold: Threshold for ICP convergence.
-    :param max_iterations: Maximum number of iterations for ICP.
-    :param initial_transformation: Initial transformation matrix (4x4 numpy array) or None for identity matrix.
+    :param source_mesh: Open3D mesh object of source.
+    :param target_mesh: Open3D mesh object of target.
+    :param max_correspondence_distance: Maximum correspondence points-pair distance.
+    :param initial_transformation: Initial transformation matrix (4x4 numpy array).
     :param use_point_to_plane: Use point-to-plane ICP (more accurate but requires normals) or point-to-point.
-    :return: Aligned source point cloud as a numpy array.
+    :return: Aligned source point cloud as a numpy array and the final transformation matrix.
     """
-    # Create Open3D point cloud objects from the numpy arrays
-    source = o3d.geometry.PointCloud()
-    source.points = o3d.utility.Vector3dVector(source_points)
-    target = o3d.geometry.PointCloud()
-    target.points = o3d.utility.Vector3dVector(target_points)
+    # Sample points from the meshes
+    source = source_mesh.sample_points_poisson_disk(number_of_points)
+    target = target_mesh.sample_points_poisson_disk(number_of_points)
 
-    # Use identity matrix as the initial transformation if none provided
-    if initial_transformation is None:
-        initial_transformation = np.eye(4)
+    # # Transfer normals from the meshes to the sampled point clouds
+    # transfer_normals_from_mesh_to_point_cloud(source_mesh, source)
+    # transfer_normals_from_mesh_to_point_cloud(target_mesh, target)
+    
+    # Estimate normals for the sampled point clouds
+    source.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
+    target.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
 
     # Choose the ICP method: point-to-plane or point-to-point
-    if use_point_to_plane:
-        # Compute normals for the point clouds for point-to-plane ICP
-        source.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=threshold * 2, max_nn=30))
-        target.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=threshold * 2, max_nn=30))
-        registration_method = o3d.pipelines.registration.TransformationEstimationPointToPlane()
-    else:
-        registration_method = o3d.pipelines.registration.TransformationEstimationPointToPoint()
+    registration_method = o3d.pipelines.registration.TransformationEstimationPointToPlane() if use_point_to_plane else o3d.pipelines.registration.TransformationEstimationPointToPoint()
+
+    # Set the ICP convergence criteria
+    criteria = o3d.pipelines.registration.ICPConvergenceCriteria(relative_fitness=relative_fitness, relative_rmse=relative_rmse, max_iteration=max_iteration)
 
     # Apply ICP to align the point clouds
     icp_result = o3d.pipelines.registration.registration_icp(
-        source, target, threshold, initial_transformation,
-        registration_method,
-        o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=max_iterations)
+        source, target, max_correspondence_distance, initial_transformation,
+        registration_method, criteria
     )
 
-    # Apply the final transformation to the source points
-    transformed_points = np.dot(np.hstack((source_points, np.ones((source_points.shape[0], 1)))), icp_result.transformation.T)[:, :3]
-    # output the transformation matrix as well
-    return transformed_points, icp_result.transformation
+    return icp_result.transformation
 
 def chamfer_distance(set1, set2):
     tree1, tree2 = cKDTree(set1), cKDTree(set2)
     distances1, distances2 = tree1.query(set2)[0], tree2.query(set1)[0]
     return np.mean(np.square(distances1)) + np.mean(np.square(distances2))
 
-def rotate_mesh_points(mesh_points, angle, axis):
+
+def visualize_meshes(mesh1, mesh2, title="Meshes"):
     """
-    Rotate mesh points around a given axis by a specified angle.
+    Visualize two Open3D meshes side by side.
 
-    :param mesh_points: NumPy array of mesh points.
-    :param angle: Rotation angle in radians.
-    :param axis: Axis of rotation, should be a 3D vector.
-    :return: Rotated mesh points as a NumPy array.
+    :param mesh1: Open3D mesh object.
+    :param mesh2: Open3D mesh object.
+    :param title: Title of the visualization window.
     """
-    # Normalize the axis to make it a unit vector
-    axis = axis / np.linalg.norm(axis)
-
-    # Create a rotation matrix
-    rotation_matrix = o3d.geometry.get_rotation_matrix_from_axis_angle(axis * angle)
-
-    # Apply rotation
-    rotated_points = np.dot(mesh_points, rotation_matrix.T)
-    return rotated_points
-
-def visualize_meshes(mesh1_points, mesh2_points, title="Mesh Visualization"):
-    """
-    Visualize two sets of mesh points.
-
-    :param mesh1_points: First set of mesh points.
-    :param mesh2_points: Second set of mesh points.
-    :param title: Window title.
-    """
-    # Create point clouds
-    mesh1_cloud = o3d.geometry.PointCloud()
-    mesh1_cloud.points = o3d.utility.Vector3dVector(mesh1_points)
-    mesh1_cloud.paint_uniform_color([1, 0, 0])  # Red color
-
-    mesh2_cloud = o3d.geometry.PointCloud()
-    mesh2_cloud.points = o3d.utility.Vector3dVector(mesh2_points)
-    mesh2_cloud.paint_uniform_color([0, 1, 0])  # Green color
+    # Visualize the meshes
+    o3d.visualization.draw_geometries([mesh1, mesh2], window_name=title)
     
-    # Red is prediction, green is ground truth
-    title = f"{title} (Red: Prediction, Green: Ground Truth)"
+def transform_mesh(mesh,transformation_matirx):
+    mesh.transform(transformation_matirx)
+    return mesh
 
-    # Visualize
-    o3d.visualization.draw_geometries([mesh1_cloud, mesh2_cloud], window_name=title)
-
-def normalize_mesh_scale(mesh_points):
+def sample_points_from_mesh(mesh, number_of_points=1000, sampling_method='poisson_disk'):
     """
-    Normalize the scale of mesh points to fit within a unit cube centered at the origin.
+    Sample points from an Open3D mesh.
 
-    :param mesh_points: NumPy array of mesh points.
-    :return: Scaled mesh points as a NumPy array.
+    :param mesh: Open3D mesh object.
+    :param number_of_points: Number of points to sample.
+    :param sampling_method: Sampling method to use. Options: 'uniform' or 'poisson_disk'.
+    :return: Open3D point cloud object.
     """
-    # Calculate the bounding box of the points
-    min_bound = np.min(mesh_points, axis=0)
-    max_bound = np.max(mesh_points, axis=0)
+    if sampling_method == 'uniform':
+        return mesh.sample_points_uniformly(number_of_points)
+    elif sampling_method == 'poisson_disk':
+        return mesh.sample_points_poisson_disk(number_of_points)
+    else:
+        raise ValueError(f"Sampling method {sampling_method} not supported.")
+
+def normalize_mesh_scale(mesh):
+    """
+    Normalize the scale of an Open3D mesh to fit within a unit cube centered at the origin.
+
+    :param mesh: Open3D mesh object.
+    :return: Scaled Open3D mesh object.
+    """
+    # Calculate the bounding box of the mesh
+    min_bound = np.asarray(mesh.get_min_bound())
+    max_bound = np.asarray(mesh.get_max_bound())
 
     # Calculate the scale factor
     scale = np.max(max_bound - min_bound)
     if scale == 0:
-        return mesh_points
+        return mesh
 
-    # Normalize the mesh points
-    normalized_points = (mesh_points - min_bound) / scale
-    return normalized_points - np.mean(normalized_points, axis=0)
+    # Normalize the mesh scale and center it at the origin
+    mesh.scale(1 / scale, center=min_bound)
+    mesh.translate(-mesh.get_center())
+
+    return mesh
+
 
 
 
